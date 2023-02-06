553689455daf9321ef0286f38bc57be8058e02f1
Kyle Simmons <simmonskyleb@gmail.com>
2023-02-05 22:08:22 -0700

    able to fetch coords

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,15 @@
-var myString="Hello World!"
-function log2console(myString:string):void{console.log(myString)}
-log2console(myString)
+$('#searchButton').on("click",fetchCoords)
+
+function fetchCoords(){
+    let cityName = $('#searchField').val()
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=b70ad42d9781f26332d6aa51e4e2722e`)
+    .then(response => response.json().then(data => fetchWeather(data)))
+}
+
+function fetchWeather(data:Array<object>){
+    try {
+        let lat = data[0].lat
+        let lon = data[0].lon
+        console.log(`${lat},${lon}`)
+    }catch {(e:any) => console.log(e)}
+}
