031b3b806afff3d952593b16b9244d00908f1f6f
Kyle Simmons <simmonskyleb@gmail.com>
2023-02-07 13:57:04 -0700

    try/catch handling

--- build/script.js
+++ build/script.js
@@ -1,17 +1,23 @@
 "use strict";
 $('#searchButton').on("click", fetchCoords);
 function fetchCoords() {
-    let cityName = $('#searchField').val();
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=b70ad42d9781f26332d6aa51e4e2722e`)
-        .then(response => response.json().then(data => fetchWeather(data)));
+    try {
+        let cityName = $('#searchField').val();
+        if (cityName == "")
+            throw "City name cannot be blank";
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=b70ad42d9781f26332d6aa51e4e2722e`)
+            .then(response => response.json()
+            .then(data => fetchWeather(data))
+            .catch(e => { alert(e); }));
+    }
+    catch (e) {
+        alert(e);
+    }
 }
 function fetchWeather(data) {
-    try {
-        let lat = data[0].lat;
-        let lon = data[0].lon;
-        console.log(`${lat},${lon}`);
-    }
-    catch {
-        (e) => console.log(e);
-    }
+    if (data[0] == undefined)
+        throw "Cannot find city";
+    let lat = data[0].lat;
+    let lon = data[0].lon;
+    console.log(`${lat},${lon}`);
 }
--- src/script.ts
+++ src/script.ts
@@ -1,15 +1,20 @@
 $('#searchButton').on("click",fetchCoords)
 
 function fetchCoords(){
-    let cityName = $('#searchField').val()
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=b70ad42d9781f26332d6aa51e4e2722e`)
-    .then(response => response.json().then(data => fetchWeather(data)))
+    try {
+        let cityName = $('#searchField').val()
+        if (cityName == "") throw "City name cannot be blank"
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=b70ad42d9781f26332d6aa51e4e2722e`)
+            .then(response => response.json()
+            .then(data => fetchWeather(data))
+            .catch(e => {alert(e)}))
+    }catch (e) {alert(e)}
 }
 
-function fetchWeather(data:Array<object>){
-    try {
-        let lat = data[0].lat
-        let lon = data[0].lon
-        console.log(`${lat},${lon}`)
-    }catch {(e:any) => console.log(e)}
+function fetchWeather(data:Array<any>){
+    if (data[0] == undefined)
+        throw "Cannot find city";
+    let lat = data[0].lat
+    let lon = data[0].lon
+    console.log(`${lat},${lon}`)
 }
