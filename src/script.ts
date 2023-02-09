be7bde0c9b3dd26995d184b206b853c06b024b47
Kyle Simmons <simmonskyleb@gmail.com>
2023-02-08 18:45:26 -0700

    switch thru icons
    will probably be refactored, assuming other API for current day has same object structure

--- src/script.ts
+++ src/script.ts
@@ -22,7 +22,32 @@ function fetchWeather(){
     
     .then(data => {
         for(let i=0; i<40; i+=8){ //Jump 8*3hrs (24hrs)
-            console.log(data.list[i])
+            let date = (data.list[i].dt_txt).substr(5,5)
+            let iconID = data.list[i].weather[0].icon
+            switch (iconID){
+                case '01d':
+                    var icon='☀';break
+                case '02d':
+                    var icon='🌤';break
+                case '03d':
+                    var icon='⛅';break
+                case '04d':
+                    var icon='☁';break
+                case '09d':
+                    var icon='🌦';break
+                case '10d':
+                    var icon='🌧';break
+                case '11d':
+                    var icon='⛈';break
+                case '13d':
+                    var icon='🌨';break
+                case '50d':
+                    var icon='🌫';break
+                default:
+                    var icon='❔'
+            }
+            console.log(date)
+            console.log(icon)
         }
     })
     .catch(e=>{return alert(e)})
